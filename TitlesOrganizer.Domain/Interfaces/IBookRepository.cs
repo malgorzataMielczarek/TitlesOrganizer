11cf07a158229d6c2e1e1c0772c3dcf150827881
mielczarek <malgorzata.mielczarek@unityline.pl>
2023-08-02 18:18:53 +0200

    Add example repository, its contract and database connection string configuration

---
+++ TitlesOrganizer.Domain/Interfaces/IBookRepository.cs
@@ -0,0 +1,15 @@
+﻿using TitlesOrganizer.Domain.Models;
+
+namespace TitlesOrganizer.Domain.Interfaces
+{
+    public interface IBookRepository
+    {
+        int AddBook(Book book);
+
+        void DeleteBook(int bookId);
+
+        Book? GetBookById(int bookId);
+
+        IQueryable<Book> GetBooksByAuthor(int authorId);
+    }
+}
---
+++ TitlesOrganizer.Infrastructure/Repositories/BookRepository.cs
@@ -0,0 +1,43 @@
+﻿using TitlesOrganizer.Domain.Interfaces;
+using TitlesOrganizer.Domain.Models;
+
+namespace TitlesOrganizer.Infrastructure.Repositories
+{
+    public class BookRepository : IBookRepository
+    {
+        private readonly Context _context;
+
+        public BookRepository(Context context)
+        {
+            _context = context;
+        }
+
+        public int AddBook(Book book)
+        {
+            _context.Books.Add(book);
+            _context.SaveChanges();
+
+            return book.Id;
+        }
+
+        public void DeleteBook(int bookId)
+        {
+            Book? book = _context.Books.Find(bookId);
+            if (book != null)
+            {
+                _context.Books.Remove(book);
+                _context.SaveChanges();
+            }
+        }
+
+        public Book? GetBookById(int bookId)
+        {
+            return _context.Books.FirstOrDefault(book => book.Id == bookId);
+        }
+
+        public IQueryable<Book> GetBooksByAuthor(int authorId)
+        {
+            return _context.Books.Where(b => b.BookAuthors.Select(ba => ba.AuthorId).Contains(authorId));
+        }
+    }
+}
