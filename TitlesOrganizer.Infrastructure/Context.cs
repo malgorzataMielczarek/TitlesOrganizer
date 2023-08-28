5d6a25b100b6bfe8ed183510d9ce6e659daa0a4b
mielczarek <malgorzata.mielczarek@unityline.pl>
2023-08-28 16:56:37 +0200

    Add data types or max length for db columns

--- TitlesOrganizer.Infrastructure/Context.cs
+++ TitlesOrganizer.Infrastructure/Context.cs
@@ -28,12 +28,59 @@ namespace TitlesOrganizer.Infrastructure
         {
             base.OnModelCreating(modelBuilder);
 
+            // Relations
             modelBuilder.Entity<Movie>()
                 .HasMany(m => m.Countries)
                 .WithMany();
             modelBuilder.Entity<TvSeries>()
                 .HasMany(s => s.Countries)
                 .WithMany();
+
+            // Data types
+            modelBuilder.Entity<Author>().Property(a => a.Name).HasMaxLength(50);
+            modelBuilder.Entity<Author>().Property(a => a.LastName).HasMaxLength(50);
+
+            modelBuilder.Entity<Book>().Property(b => b.Title).HasMaxLength(450);
+            modelBuilder.Entity<Book>().Property(b => b.OriginalTitle).HasMaxLength(450);
+            modelBuilder.Entity<Book>().Property(b => b.Edition).HasMaxLength(50);
+            modelBuilder.Entity<Book>().Property(b => b.Description).HasMaxLength(4000);
+
+            modelBuilder.Entity<BookSeries>().Property(bs => bs.Title).HasMaxLength(450);
+            modelBuilder.Entity<BookSeries>().Property(bs => bs.OriginalTitle).HasMaxLength(450);
+            modelBuilder.Entity<BookSeries>().Property(bs => bs.Description).HasMaxLength(4000);
+
+            modelBuilder.Entity<Country>().Property(c => c.Code).HasColumnType("char(3)");
+            modelBuilder.Entity<Country>().Property(c => c.Name).HasColumnType("varchar(25)");
+
+            modelBuilder.Entity<Director>().Property(d => d.Name).HasMaxLength(50);
+            modelBuilder.Entity<Director>().Property(d => d.LastName).HasMaxLength(50);
+
+            modelBuilder.Entity<Episode>().Property(e => e.Title).HasMaxLength(450);
+            modelBuilder.Entity<Episode>().Property(e => e.OriginalTitle).HasMaxLength(450);
+            modelBuilder.Entity<Episode>().Property(e => e.Description).HasMaxLength(4000);
+
+            modelBuilder.Entity<Language>().Property(lang => lang.Code).HasColumnType("char(3)");
+            modelBuilder.Entity<Language>().Property(lang => lang.Name).HasColumnType("varchar(25)");
+
+            modelBuilder.Entity<LiteratureGenre>().Property(lg => lg.Name).HasMaxLength(50);
+
+            modelBuilder.Entity<Movie>().Property(m => m.Title).HasMaxLength(450);
+            modelBuilder.Entity<Movie>().Property(m => m.OriginalTitle).HasMaxLength(450);
+            modelBuilder.Entity<Movie>().Property(m => m.Description).HasMaxLength(4000);
+
+            modelBuilder.Entity<MovieSeries>().Property(ms => ms.Title).HasMaxLength(450);
+            modelBuilder.Entity<MovieSeries>().Property(ms => ms.OriginalTitle).HasMaxLength(450);
+            modelBuilder.Entity<MovieSeries>().Property(ms => ms.Description).HasMaxLength(4000);
+
+            modelBuilder.Entity<Season>().Property(s => s.Title).HasMaxLength(450);
+            modelBuilder.Entity<Season>().Property(s => s.OriginalTitle).HasMaxLength(450);
+            modelBuilder.Entity<Season>().Property(s => s.Description).HasMaxLength(4000);
+
+            modelBuilder.Entity<TvSeries>().Property(ts => ts.Title).HasMaxLength(450);
+            modelBuilder.Entity<TvSeries>().Property(ts => ts.OriginalTitle).HasMaxLength(450);
+            modelBuilder.Entity<TvSeries>().Property(ts => ts.Description).HasMaxLength(4000);
+
+            modelBuilder.Entity<VideoGenre>().Property(vg => vg.Name).HasMaxLength(50);
         }
     }
 }
