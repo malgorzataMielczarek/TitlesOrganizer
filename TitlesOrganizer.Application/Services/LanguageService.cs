1bdef6794a2527911f4f5f3755610f88f1d5d345
mielczarek <malgorzata.mielczarek@unityline.pl>
2023-09-05 13:54:55 +0200

    Implement Language service and mappings

---
+++ TitlesOrganizer.Application/Services/LanguageService.cs
@@ -0,0 +1,25 @@
+﻿using TitlesOrganizer.Application.Interfaces;
+using TitlesOrganizer.Application.Mapping;
+using TitlesOrganizer.Application.ViewModels.LanguageVMs;
+using TitlesOrganizer.Domain.Interfaces;
+
+namespace TitlesOrganizer.Application.Services
+{
+    public class LanguageService : ILanguageService
+    {
+        private readonly ILanguageRepository _languageRepository;
+
+        public ListLanguageForListVM GetAllLanguagesForList()
+        {
+            var languages = _languageRepository.GetAllLanguages().OrderBy(l => l.Name).Select(LanguageMappings.ToLanguageForListVM).ToList();
+
+            var list = new ListLanguageForListVM()
+            {
+                Languages = languages,
+                Count = languages.Count
+            };
+
+            return list;
+        }
+    }
+}
