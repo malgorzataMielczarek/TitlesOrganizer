d3dcd160e0d2d6ed1a248298883079ee3e6eb98b
mielczarek <malgorzata.mielczarek@unityline.pl>
2023-11-14 15:08:06 +0100

    Add migrations (update database)

--- TitlesOrganizer.Infrastructure/Migrations/20231114135205_ExistingUpdate.Designer.cs
+++ TitlesOrganizer.Infrastructure/Migrations/20231114135205_ExistingUpdate.Designer.cs
@@ -3,6 +3,7 @@ using System;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using TitlesOrganizer.Infrastructure;
 
@@ -11,9 +12,11 @@ using TitlesOrganizer.Infrastructure;
 namespace TitlesOrganizer.Infrastructure.Migrations
 {
     [DbContext(typeof(Context))]
-    partial class ContextModelSnapshot : ModelSnapshot
+    [Migration("20231114135205_ExistingUpdate")]
+    partial class ExistingUpdate
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
---
+++ TitlesOrganizer.Infrastructure/Migrations/20231114135205_ExistingUpdate.cs
@@ -0,0 +1,82 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace TitlesOrganizer.Infrastructure.Migrations
+{
+    /// <inheritdoc />
+    public partial class ExistingUpdate : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_Books_BookSeries_BookSeriesId",
+                table: "Books");
+
+            migrationBuilder.RenameColumn(
+                name: "BookSeriesId",
+                table: "Books",
+                newName: "SeriesId");
+
+            migrationBuilder.RenameIndex(
+                name: "IX_Books_BookSeriesId",
+                table: "Books",
+                newName: "IX_Books_SeriesId");
+
+            migrationBuilder.AlterColumn<string>(
+                name: "Title",
+                table: "BookSeries",
+                type: "nvarchar(450)",
+                maxLength: 450,
+                nullable: false,
+                defaultValue: "",
+                oldClrType: typeof(string),
+                oldType: "nvarchar(450)",
+                oldMaxLength: 450,
+                oldNullable: true);
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Books_BookSeries_SeriesId",
+                table: "Books",
+                column: "SeriesId",
+                principalTable: "BookSeries",
+                principalColumn: "Id");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_Books_BookSeries_SeriesId",
+                table: "Books");
+
+            migrationBuilder.RenameColumn(
+                name: "SeriesId",
+                table: "Books",
+                newName: "BookSeriesId");
+
+            migrationBuilder.RenameIndex(
+                name: "IX_Books_SeriesId",
+                table: "Books",
+                newName: "IX_Books_BookSeriesId");
+
+            migrationBuilder.AlterColumn<string>(
+                name: "Title",
+                table: "BookSeries",
+                type: "nvarchar(450)",
+                maxLength: 450,
+                nullable: true,
+                oldClrType: typeof(string),
+                oldType: "nvarchar(450)",
+                oldMaxLength: 450);
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Books_BookSeries_BookSeriesId",
+                table: "Books",
+                column: "BookSeriesId",
+                principalTable: "BookSeries",
+                principalColumn: "Id");
+        }
+    }
+}
